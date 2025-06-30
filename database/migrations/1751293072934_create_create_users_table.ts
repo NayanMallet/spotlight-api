b7367dc71d1af97102a7263d81615bfffd19cb37
NotGrafik <pablo.jeanlouis@ynov.com>
2025-06-30 16:19:14 +0200

    feat(migrations): create users and events tables
    - Added migration for creating the 'users' table with fields for full name, email, password, and OAuth details.
    - Added migration for creating the 'events' table with fields for title, description, dates, location, and event types.

--- database/migrations/1751293072934_create_create_users_table.ts
+++ database/migrations/1751293072934_create_create_users_table.ts
@@ -1,11 +1,11 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
-export default class extends BaseSchema {
+export default class CreateUsersTable extends BaseSchema {
   protected tableName = 'users'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
+      table.increments('id')
       table.string('full_name').notNullable()
       table.string('email').notNullable().unique()
       table.string('banner_url').nullable()
--- database/migrations/1751293108379_create_events_table.ts
+++ database/migrations/1751293108379_create_events_table.ts
@@ -1,29 +1,25 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
-import { EventType, EventSubtype } from '#events/enums/events'
 
-export default class extends BaseSchema {
+export default class CreateEventsTable extends BaseSchema {
   protected tableName = 'events'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
+      table.increments('id')
       table.string('title').notNullable()
       table.text('description').nullable()
       table.string('banner_url').nullable()
       table.date('start_date').notNullable()
-      table.date('end_date').notNullable()
-      table.dateTime('start_hour').notNullable()
+      table.date('end_date').nullable()
+      table.dateTime('start_hour').nullable()
       table.dateTime('open_hour').nullable()
-      table.float('latitude').notNullable()
-      table.float('longitude').notNullable()
+      table.float('latitude', 8, 2).notNullable()
+      table.float('longitude', 8, 2).notNullable()
       table.string('place_name').notNullable()
       table.string('address').notNullable()
       table.string('city').notNullable()
-
-      // Remplacement des relations par des enums
-      table.enu('type', Object.values(EventType)).notNullable()
-      table.enu('subtype', Object.values(EventSubtype)).notNullable()
-
+      table.enu('type', ['concert', 'festival', 'exhibition', 'conference']).notNullable()
+      table.enu('subtype', ['rock', 'hiphop', 'jazz', 'techno', 'classical']).notNullable()
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
     })
