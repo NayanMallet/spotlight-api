711fa2a90b2757521b78e007e8d5818b561fe302
NayanMallet <nayanmallet@gmail.com>
2026-05-26 16:07:25 +0200

    📦 feat(tests): add functional API tests and improve test configuration

--- bin/test.ts
+++ bin/test.ts
@@ -45,14 +45,12 @@ new Ignitor(APP_ROOT, { importer: IMPORTER })
   .configure(async (app) => {
     const { runnerHooks, ...config } = await import('../tests/bootstrap.js')
 
-    processCLIArgs(process.argv.splice(2))
+    processCLIArgs(process.argv.slice(2))
     configure({
       ...app.rcFile.tests,
       ...config,
-      ...{
-        setup: runnerHooks.setup,
-        teardown: runnerHooks.teardown.concat([() => app.terminate()]),
-      },
+      setup: runnerHooks.setup,
+      teardown: runnerHooks.teardown.concat([() => app.terminate()]),
     })
   })
   .run(() => run())
